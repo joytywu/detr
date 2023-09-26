2adacad6790d7b2ea301f0e70bc0649cc0b326ac
Joy Wu <joytywu@gmail.com>
2023-09-26 13:01:42 -0700

    alternative input for detr_likelihood projections

--- detr_likelihood_projections.py
+++ detr_likelihood_projections.py
@@ -13,6 +13,32 @@ import torch, torchvision
 import torchvision.transforms as T
 
 
+# find original petct dicom nested study paths 
+def find_studies(path_to_data, search_string = 'PETCT_'):
+    # find all studies
+    root = plb.Path(path_to_data)
+    patient_dirs = list(root.glob('{}*'.format(search_string)))
+    
+    study_dirs = []
+
+    for dir in patient_dirs:
+        sub_dirs = list(dir.glob('*'))
+        #print(sub_dirs)
+        study_dirs.extend(sub_dirs)
+    study_dirs.sort()
+    return study_dirs
+
+
+# will need to get the latest dir with annotations
+def _get_lastest_dir(data_path):
+    dirs = list(filter(os.path.isdir, glob.glob(data_path + "/*")))
+    dirs.sort(key=lambda x: os.path.getmtime(x), reverse=True)
+    if len(dirs)>0:
+        return dirs[0]
+    else:
+        return None
+    
+
 # adapted this so doesn't need to crop/reschale again.
 def create_mipNIFTI_from_3D(img, nb_image=48):
     ls_mip=[]
@@ -214,10 +240,16 @@ def load_model(num_classes, model_path):
     return model.eval()
 
 
-def run_workflow(data_in_root, pet_dir, data_out_root, model_path, transform, finetuned_classes):
+def run_workflow_from_mip(data_in_root, pet_dir, data_out_root, model_path, transform, finetuned_classes):
+    if not os.path.isdir(data_out_root):
+        os.makedirs(data_out_root)
+        os.makedirs(os.path.join(data_out_root,'NIFTI'))
+        os.makedirs(os.path.join(data_out_root,'JSON'))
+    
     num_classes = len(finetuned_classes)
     
     #root = plb.Path(data_in_root/plb.Path('petmr_detr_dataset/test/SUV_MIP/'))
+    # study paths to mip dir
     study_paths = list(data_in_root.glob('*[Ss][Uu][Vv]*.nii.gz'))
 #     study_paths = list(root.glob('*.nii.gz'))
 #     study_paths = [x for x in study_paths if 'suv' in str(x).lower()]
@@ -248,7 +280,53 @@ def run_workflow(data_in_root, pet_dir, data_out_root, model_path, transform, fi
         with open(json_outpath,"w") as outfile:
             json.dump(bboxes_dict, outfile, cls=NpEncoder)
             print('Saved results:', mip_path)
-        
+
+
+def run_workflow_from_nested_pet(data_in_root, data_out_root, model_path, transform, finetuned_classes, num_angles = 12):
+    # load OD model
+    num_classes = len(finetuned_classes)
+    model = load_model(num_classes, model_path)
+    
+    for split in ['train','val']:
+        data_root = os.path.join(data_in_root, split)
+        out_path = os.path.join(data_out_root, split)
+        os.makedirs(os.path.join(data_out_root, split,'MIP_SUV'),exist_ok=True)
+        os.makedirs(os.path.join(data_out_root, split,'NIFTI'),exist_ok=True)
+        os.makedirs(os.path.join(data_out_root, split,'JSON'),exist_ok=True)
+        study_dirs = find_studies(data_root)
+        #print('###', len(study_dirs))
+
+        for study_dir in tqdm(study_dirs):
+            # relative path is the ID of the pet/ct exam
+            relative_path = plb.Path(study_dir.parent.name)/plb.Path(study_dir.name)
+            study_id = str(relative_path).replace('/','|')+'.nii.gz'
+            print(study_id)
+            pet_path = os.path.join(study_dir,'SUVres.nii.gz')
+#         seg_path = os.path.join(study_dir,'SEGres.nii.gz')
+            print('Reading pet study:', pet_path)
+            pet = nib.load(pet_path)
+            pet_img = pet.get_fdata()
+            header = pet.header
+            print('Creating and saving MIP images for pet study')
+            mip_pet = create_mipNIFTI_from_3D(pet, nb_image=num_angles)
+            print('Saving MIP data')
+            mip_outpath = os.path.join(out_path, 'MIP_SUV', '{}.nii.gz'.format(study_id))
+            nib.save(mip_pet, mip_outpath)
+            print('Tumor bbox objects detection')
+            mip_stack = mip_pet.get_fdata()
+            # num_angles = mip_stack.shape[2]
+            bboxes_dict = get_all_predicted_bboxes(mip_stack, model, suv_max = 6, angles = num_angles, threshold = 0.7)
+            print('Generating 3D Bbox likelihood back projections')
+            bbox_likelihoods_3D = get_projected_bbox_likelihood(bboxes_dict, mip_stack, pet_img)
+            likelihood_nifti = nib.Nifti1Image(bbox_likelihoods_3D, None, header)
+            print('Saving results')
+            nii_outpath = os.path.join(out_path, 'NIFTI', '{}.nii.gz'.format(study_id))
+            nib.save(likelihood_nifti, nii_outpath)
+            json_outpath = os.path.join(out_path, 'JSON', '{}.json'.format(study_id))
+            with open(json_outpath,"w") as outfile:
+                json.dump(bboxes_dict, outfile, cls=NpEncoder)
+                print('Saved results:', pet_path)
+            
 
 # https://stackoverflow.com/questions/50916422/python-typeerror-object-of-type-int64-is-not-json-serializable
 class NpEncoder(json.JSONEncoder):
@@ -267,10 +345,6 @@ if __name__ == "__main__":
     pet_dir = str(sys.argv[2]) # path to original pet nifti, e.g. /gpfs/fs0/data/stanford_data/master/image_for_train_processed/
     
     data_out_root = plb.Path(sys.argv[3])  # path to where we want to save the DETR dataset, e.g. /gpfs/fs0/data/stanford_data/petmr_detr_dataset/test/
-    if not os.path.isdir(data_out_root):
-        os.makedirs(data_out_root)
-        os.makedirs(os.path.join(data_out_root,'NIFTI'))
-        os.makedirs(os.path.join(data_out_root,'JSON'))
 
     model_path = plb.Path(sys.argv[4]) # path to trained model checkpoint, # /home/joywu/detr/outputs/baseline/checkpoint.pth
     
@@ -284,13 +358,21 @@ if __name__ == "__main__":
     finetuned_classes = [
           'N/A', 'tumor',]
     
-    run_workflow(data_in_root, pet_dir, data_out_root, model_path, transform, finetuned_classes) 
+    if pet_dir != '.':
+        # runs on already processed MIP images
+        run_workflow_from_mip(data_in_root, pet_dir, data_out_root, model_path, transform, finetuned_classes) 
+    else:
+        # process pet images to mip then run -- also assuming NCIS nested dicom directories
+        run_workflow_from_nested_pet(data_in_root, data_out_root, model_path, transform, finetuned_classes)
+    
     
     #done #python detr_likelihood_projections.py /gpfs/fs0/data/stanford_data/petmr_detr_dataset/baseline_test/SUV_MIP/ /gpfs/fs0/data/stanford_data/master/image_for_train_processed/ /gpfs/fs0/data/stanford_data/petmr_detr_dataset/baseline_test/bbox_likelihood_projections/ /home/joywu/detr/outputs/baseline/checkpoint.pth
     
     #done #python detr_likelihood_projections.py /gpfs/fs0/data/stanford_data/petmr_detr_dataset/baseline_dev/SUV_MIP/ /gpfs/fs0/data/stanford_data/master/image_for_train_processed/ /gpfs/fs0/data/stanford_data/petmr_detr_dataset/baseline_dev/bbox_likelihood_projections/ /home/joywu/detr/outputs/baseline/checkpoint.pth
     
     #done #python detr_likelihood_projections.py /gpfs/fs0/data/stanford_data/petmr_detr_dataset/followup/SUV_MIP/ /gpfs/fs0/data/stanford_data/followup_shashi/ /gpfs/fs0/data/stanford_data/petmr_detr_dataset/followup/bbox_likelihood_projections/ /home/joywu/detr/outputs/baseline/checkpoint.pth
+    
+    # python detr_likelihood_projections.py /Volumes/GazeData/data/pilot/ . /Users/joywu/Documents/Research/gaze_decoding /Users/joywu/Documents/Research/detr/outputs/baseline/checkpoint.pth
     
     
     
